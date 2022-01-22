112d9295d547aae829bdf53c37229f6958d31b9e
wangj <wangjian020426@126.com>
2022-01-22 16:09:28 +0800

    第一次提交github

---
+++ src/com/ah/cg/git.java
@@ -0,0 +1,13 @@
+package com.ah.cg;
+
+public class git {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
